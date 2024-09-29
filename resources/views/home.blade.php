afd89677998cb9b27ac45f01146f027e11cab7ea
fabiomarzano <fabiomarzano1993@gmail.com>
2024-09-29 18:21:00 +0200

    [UPD] add-control-and-foreach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,14 @@
             <div class="col-12">
                 <div class="row">
                     @foreach ($movies as $movie)
-                        <div class="card">
-                            <div class="card-title">
-                               <h3>{{ $movie->title}}</h3>
-                            </div>
-                            <div class="card-body">
-                                <p>{{ $movie->original_title }}, {{ $movie->nationality }},  {{ $movie->vote }},  {{ $movie->date }}</p>
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <div class="card">
+                                <div class="card-title">
+                                   <h3>{{ $movie->title}}</h3>
+                                </div>
+                                <div class="card-body">
+                                    <p>{{ $movie->original_title }}, {{ $movie->nationality }},  {{ $movie->vote }},  {{ $movie->date }}</p>
+                                </div>
                             </div>
                         </div>
                     @endforeach
